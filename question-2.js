f258107f834e62f926b03ae6fb2b0d6f2fbe1175
Kunakorn <kunakorn.ph98@gmail.com>
2024-05-22 15:40:29 +0700

    Js skill checkpoint March 22/05/24

--- question-2.js
+++ question-2.js
@@ -1,6 +1,14 @@
-// Question #2
+  // Question #2
 const inventory = [
   { name: "Apple", price: 35, quantity: 100 },
   { name: "Banana", price: 10, quantity: 50 },
 ];
 // เริ่มเขียนโค้ดตรงนี้
+inventory[0].quantity = 200
+inventory.push({name: "Orange", price: 20, quantity: 300})
+console.log(inventory)
+let totalPrice = 0
+for (let i = 0; i < inventory.length; i++) {
+  totalPrice += inventory[i].price * inventory[i].quantity 
+}
+console.log(totalPrice)
--- question-4.js
+++ question-4.js
@@ -1,7 +1,20 @@
 // Question #4
+let checkLeastIndex = {}
 const inventory = [
   { name: "Apple", price: 35, quantity: 100 },
   { name: "Banana", price: 10, quantity: 50 },
   { name: "Orange", price: 30, quantity: 60 },
 ];
 // เริ่มเขียนโค้ดตรงนี้
+
+const leastPrice = function check(item){
+  checkLeastIndex = item[0].quantity
+  console.log(checkLeastIndex)
+  for (let i = 1; i < item.length; i++) {
+    if(checkLeastIndex> item[i].quantity){
+      checkLeastIndex = item[i]
+    }
+    return `สินค้าที่มีจำนวนต่ำที่สุดในคลังสินค้าคือ ${checkLeastIndex.name} ซึ่งมี ${checkLeastIndex.quantity} ชิ้น`
+  }
+}
+console.log(leastPrice(inventory))
